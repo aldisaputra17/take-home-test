bc394fe5a85fe0045c7c562e15702cce1f2e0865
aldisaputra17 <aldisaput17@gmail.com>
2023-01-29 20:34:22 +0700

    soal no 1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aldisaputra17/take-home-test
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aldisaputra17/take-home-test/mataUang"
+)
+
+func main() {
+	fmt.Println(mataUang.PecahanMataUang(16000))
+}
